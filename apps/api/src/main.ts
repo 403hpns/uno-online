e1540fbe13c884ad45826ec41a71065235f9e7d0
403hpns <dev.403hpns@pm.me>
2025-01-31 15:23:09 +0100

    feat(api): add custom ws adapter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SocketIoAdapter } from './ws-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+  });
+
+  app.useWebSocketAdapter(new SocketIoAdapter(app));
+
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
